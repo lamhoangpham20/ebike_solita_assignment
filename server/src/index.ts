9f5e42769580b65768fadda0120ab37404fa06ee
lam <lamhoangpham20@gmail.com>
2022-07-29 13:32:11 +0300

    initialize express ts

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (request: any, response: { json: (arg0: { info: string; }) => void; }) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
