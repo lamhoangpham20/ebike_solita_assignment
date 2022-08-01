2bdd22e259fc9384ed4a6e415aeb56fbe5012f8c
lam <lamhoangpham20@gmail.com>
2022-08-01 21:47:00 +0300

    add station CRUD

--- server/src/resolvers/station.ts
+++ server/src/resolvers/station.ts
@@ -1,7 +1,20 @@
 import { myDataSource } from "../app-data-source";
 import { Station } from "../entities/Station";
 
-async function getStations() {
+type StationInput = {
+  id: string;
+  name?: string;
+  namn?: string;
+  nimi?: string;
+  oisoite?: string;
+  address?: string;
+  operator?: string;
+  capacities?: number;
+  longitude?: number;
+  latitude?: number;
+};
+
+async function getStations(): Promise<Station[] | null> {
   const stations = await myDataSource
     .getRepository(Station)
     .createQueryBuilder("s")
@@ -11,9 +24,53 @@ async function getStations() {
   return stations;
 }
 
-async function getStationbyId(id: string) {
-  const stations = Station.findOne({ where: { id } });
+async function getStationbyId(id: string): Promise<Station | null> {
+  const stations = await Station.findOne({ where: { id } });
   return stations;
 }
 
-export { getStations, getStationbyId };
+async function createStation(input: StationInput): Promise<Station | null> {
+  const foundStation = await Station.findOneBy({ id: input.id });
+  if (foundStation) {
+    return null;
+  }
+  return Station.create({ ...input }).save();
+}
+
+async function updateStation(input: StationInput): Promise<Station | null> {
+  const id = input.id;
+  const station = await Station.findOneBy({ id });
+  if (!station) {
+    return null;
+  }
+  if (typeof input.name !== undefined) {
+    await Station.update(
+      { id },
+      {
+        name: input.name,
+        address: input.address,
+        capacities: input.capacities,
+        longitude: input.longitude,
+        latitude: input.latitude,
+      }
+    );
+  }
+  return station;
+}
+
+async function deleteStation(id: string) {
+  const station = await Station.findOneBy({ id });
+  if (!station) {
+    return null;
+  }
+  await Station.delete({ id });
+  return true;
+}
+
+export {
+  getStations,
+  getStationbyId,
+  createStation,
+  updateStation,
+  deleteStation,
+};
--- server/src/routes/station.ts
+++ server/src/routes/station.ts
@@ -1,9 +1,15 @@
 const express = require("express");
 import { Response, Request } from "express";
-import { getStations, getStationbyId } from "../resolvers/station";
+import {
+  getStations,
+  getStationbyId,
+  createStation,
+  updateStation,
+  deleteStation,
+} from "../resolvers/station";
 const router = express.Router();
 
-router.get("/", async function (res: Response) {
+router.get("/", async function (_: Request, res: Response) {
   const stations = await getStations();
   res.json(stations);
 });
@@ -12,6 +18,40 @@ router.get("/:id", async function (req: Request, res: Response) {
   const id = req.params.id;
   const station = await getStationbyId(id);
   res.json(station);
+});
+
+router.post("/", async function (req: Request, res: Response) {
+  const input = req.body;
+  const result = await createStation(input).catch((err) => {
+    res.json(err);
+  });
+  console.log(result);
+  if (!result) {
+    res.json(false);
+  }
+  res.json(result?.id);
+});
+
+router.put("/", async function (req: Request, res: Response) {
+  const input = req.body;
+  const result = await updateStation(input).catch((err) => {
+    res.json(err);
+  });
+  if (!result) {
+    res.json(false);
+  }
+  res.sendStatus(201);
+});
+
+router.delete("/:id", async function (req: Request, res: Response) {
+  const id = req.params.id;
+  const result = await deleteStation(id).catch((err) => {
+    res.json(err);
+  });
+  if (!result) {
+    res.json(false);
+  }
+  res.sendStatus(201);
 });
 
 module.exports = router;
